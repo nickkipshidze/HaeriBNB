3cd9ef48f4ceb421d6f28494fdce07b683da00e9
NickKipshidze <kipshidze.nick@gmail.com>
2024-02-26 21:32:49 +0400

    Add Python & NPM checking

--- start.sh
+++ start.sh
@@ -8,21 +8,47 @@ if [ -z "$1" ]; then
 fi
 
 if [ "$1" = "backend" ]; then
+    echo "[start] Checking python..."
+
+    if command -v python3 &>/dev/null; then
+        echo "[start] Python3 is installed - version: $(python3 --version)"
+    else
+        echo "[start] Python3 is not installed"
+        exit 1
+    fi
+
     echo "[start] Starting backend server"
+
     cd ./backend
     python3 manage.py migrate
     python3 manage.py runserver localhost:8000
+
 elif [ "$1" = "frontend" ]; then
+    echo "[start] Checking npm..."
+
+    if command -v npm &>/dev/null; then
+        echo "[start] npm is installed - version: $(npm --version)"
+    else
+        echo "[start] npm is not installed"
+        exit 1
+    fi
+
     echo "[start] Starting frontend server"
+
     cd ./frontend
     npm run dev
+
 elif [ "$1" = "cleanup" ]; then
     echo "[start] Cleaning up..."
+
     find . -type d -name "__pycache__" -exec rm -r {} +
     rm -rf ./frontend/.next
+
 elif [ "$1" = "count" ]; then
     echo "[start] Counting lines of code..."
+
     git ls-files -- "*.py" "*.js" "*.css" "*.txt" "*.md" "*.sh" | xargs wc -l
+
 else
     echo "[start] Invalid argument: $1"
     exit 1
