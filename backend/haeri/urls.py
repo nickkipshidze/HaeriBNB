6be3be6b1e02f164a70029c0d2880edcf5231242
NickKipshidze <kipshidze.nick@gmail.com>
2024-02-28 10:54:11 +0400

    Include accounts.urls at auth/

--- backend/haeri/urls.py
+++ backend/haeri/urls.py
@@ -6,6 +6,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("accounts.urls"), name="auth"),
     path("api/schema", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs", SpectacularSwaggerView.as_view(url_name="schema"))
 ] \
